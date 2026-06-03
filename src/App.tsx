d647d9435eabe3ca4aa490aeeca15713286599af
Autumn-dev <nathanhawkii2@gmail.com>
2026-06-03 22:44:30 +0100

    First basic version

--- src/App.tsx
+++ src/App.tsx
@@ -1,126 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [headValue, setheadValue] = useState(0);
+  const [randomNo, setRandomNo] = useState(0);
+  const coinFlip = () => {
+    const random = Math.floor(Math.random() * 101);
+    setRandomNo(random);
+    const isHead = randomNo <= headValue;
+    alert(
+      isHead
+        ? `You got heads! (You rolled ${randomNo})`
+        : `You got tails! (You rolled ${randomNo})`,
+    );
+  };
   return (
     <>
       <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
         <div>
           <h1>Totally not biased 50-50 Coinflip</h1>
           <p>
-            User can influence the coinflip odds, simply edit the chance for a heads.
+            User can influence the coinflip odds, simply edit the chance for a
+            heads.
           </p>
         </div>
         <div>
-        <label for="heads_rate">What percentage should we be hitting heads?</label>
-        <input type="number" name="heads_rate" min="0" max="100" placeholder="50"></input>
+          <label htmlFor="heads_rate">
+            What percentage should we be hitting heads?
+          </label>
+          <br></br>
+          <input
+            type="number"
+            name="heads_rate"
+            value={headValue}
+            min="0"
+            max="100"
+            placeholder="50"
+            onChange={(e) => setheadValue(Number(e.target.value))}
+          ></input>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
+        <div>
+          <p>
+            This means that the current chance of tails is: {100 - headValue}%
+          </p>
         </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
+        <div>
+          <button onClick={coinFlip}>Hit that shit!</button>
         </div>
       </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
